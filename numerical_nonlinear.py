52b3768981124d19dd88a9cfda1ac9bf0b1d241e
Dudzin Dzmitry <dmitriy.i.dudin@gmail.com>
2023-11-06 15:14:55 +0300

    feat: added some placeholders for new methods

--- numerical_nonlinear.py
+++ numerical_nonlinear.py
@@ -1,12 +1,33 @@
-from numpy.linalg import norm, solve
+from numpy.linalg import solve
 import numpy as np
 
-def newton(F, J, x0, tol = 1.e-10, max_iter=20):
+def fixedpoint(g, x0, tol=1.e-8, max_iter=30):
+    x = x0
+    for _ in range(max_iter):
+        x_old = x
+        x = g(x)
+        # if abs(x-x_old) < tol: break
+    return x
+
+def newton(F, J, x0, tol = 1.e-8, max_iter=30):
     x = x0
     for _ in range(max_iter):
         fx = F(x)
-        if norm(fx, np.inf) < tol: break
         Jx = J(x)
-        delta = solve(Jx, -fx) 
-        x = x + delta            
+        delta = solve(Jx, -fx)
+        # x_old = x
+        # x = x + delta
+        # if abs(x-x_old) < tol: break
     return x
+
+
+def constJnewton(F, J, x0, tol = 1.e-8, max_iter=30):
+    x = x0
+    Jx = J(x)
+    for _ in range(max_iter):
+        fx = F(x)
+        delta = solve(Jx, -fx)
+        # x_old = x
+        # x = x + delta
+        # if abs(x-x_old) < tol: break
+        return x

59bf61b9e9ccf466796b5caed94ad0f49474a054
Dudzin Dzmitry <dmitriy.i.dudin@gmail.com>
2023-11-09 14:54:48 +0300

    feat: execution time

--- ode_problems.py
+++ ode_problems.py
@@ -1,5 +1,6 @@
 from numerical_ode import rungekutta
 import numpy as np
+import time
 # Решение тестового уравнения явным методом Рунге-Кутта #
 def rk3_test(method="ex rk 3"):
     # Параметры метода #
@@ -25,8 +26,11 @@ def rk3_test(method="ex rk 3"):
     f = lambda t,y: np.exp(y)*(1+t)
     df = lambda t,y: np.exp(y)*(1+t)
     ex_rk3.init_problem(f, y0, t0, tn, df)
+    st = time.time()
     # Решение заданной задачи инициализированным методом #
     t, y = ex_rk3(0.01, mtd)
+    et = time.time()
+    print(f'Время выполнения алгоритма = {et-st} секунд')
     # Отображение решения #
     ex_rk3.plot_Y(t, y)
     # Отображение изменения временных шагов #
@@ -61,8 +65,11 @@ def robertson(method="ex rk 3"):
                           0.04 - 1e4*y[2] - 6e7*y[1] - 1e4*y[1] +
                              0 +            6e7*y[1] +        0], dtype=np.float64)
     im_rk3.init_problem(f, y0, t0, tn, df)
+    st = time.time()
     # Решение заданной задачи инициализированным методом #
     t, y = im_rk3(0.0005, mtd)
+    et = time.time()
+    print(f'Время выполнения алгоритма = {et-st} секунд')
     # Отображение решения #
     im_rk3.plot_Y(t, y)
     # Отображение изменения временных шагов #
